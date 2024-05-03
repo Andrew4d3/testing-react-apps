8865fe509bb1284910958f1a4841355cfcb07a79
Andrew Davis <andrew4d3@gmail.com>
2024-05-03 09:02:20 -0400

    Lesson 173: using test-data-bot library

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -1,49 +1,32 @@
 // form testing
+// 💯 use Test Data Bot
 // http://localhost:3000/login
 
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import {build, fake} from '@jackfranklin/test-data-bot'
 import Login from '../../components/login'
-import faker from 'faker'
 
-const { internet } = faker;
-
-const buildLoginForm =  ({ password =  internet.password(), username = internet.userName() }) => ({
-  username,
-  password,
+const buildLoginForm = build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password()),
+  },
 })
 
 test('submitting the form calls onSubmit with username and password', async () => {
-  // 🐨 create a variable called "submittedData" and a handleSubmit function that
-  // accepts the data and assigns submittedData to the data that was submitted
-  // 💰 if you need a hand, here's what the handleSubmit function should do:
-  // const handleSubmit = data => (submittedData = data)
-  const { username, password } = buildLoginForm({password: 'abc'})
-  const handleSubmit = jest.fn();
-  //
-  // 🐨 render the login with your handleSubmit function as the onSubmit prop
-
+  const handleSubmit = jest.fn()
   render(<Login onSubmit={handleSubmit} />)
+  const {username, password} = buildLoginForm()
 
-  // 🐨 get the username and password fields via `getByLabelText`
-  const userNameInput = screen.getByLabelText("Username")
-  const passwordInput = screen.getByLabelText("Password")
-  // 🐨 use `await userEvent.type...` to change the username and password fields to
-  //    whatever you want
-  //
-  await userEvent.type(userNameInput, username)
-  await userEvent.type(passwordInput, password)
-  // 🐨 click on the button with the text "Submit"
+  await userEvent.type(screen.getByLabelText(/username/i), username)
+  await userEvent.type(screen.getByLabelText(/password/i), password)
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
 
-  await userEvent.click(screen.getByText("Submit"))
-  //
-  // assert that submittedData is correct
-  // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
- expect(handleSubmit).toHaveBeenCalledWith({ username, password })
+  expect(handleSubmit).toHaveBeenCalledWith({
+    username,
+    password,
+  })
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
 })
-
-/*
-eslint
-  no-unused-vars: "off",
-*/
