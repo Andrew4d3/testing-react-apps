379c97b6008b21c33bdda5c20b170935c13f81e8
Andrew Davis <andrew4d3@gmail.com>
2024-05-11 12:05:04 -0400

    Lesson 187: testing different theme

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -1,4 +1,5 @@
 // testing with context and a custom render method
+// 💯 add a test for the dark theme
 // http://localhost:3000/easy-button
 
 import * as React from 'react'
@@ -15,5 +16,17 @@ test('renders with the light styles for the light theme', () => {
   expect(button).toHaveStyle(`
     background-color: white;
     color: black;
+  `)
+})
+
+test('renders with the dark styles for the dark theme', () => {
+  const Wrapper = ({children}) => (
+    <ThemeProvider initialTheme="dark">{children}</ThemeProvider>
+  )
+  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
   `)
 })
