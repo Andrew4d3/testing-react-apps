9e69184fa207a24da4a34568a1b6dc25d52626c6
Andrew Davis <andrew4d3@gmail.com>
2024-05-11 12:15:50 -0400

    Lesson 189: putting custom RTL renders inside separate module to be imported

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -1,21 +1,13 @@
 // testing with context and a custom render method
-// 💯 create a custom render method
+// 💯 swap @testing-library/react with app test utils
 // http://localhost:3000/easy-button
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
-import {ThemeProvider} from '../../components/theme'
+import {render, screen} from 'test/test-utils'
 import EasyButton from '../../components/easy-button'
 
-function renderWithProviders(ui, {theme = 'light', ...options} = {}) {
-  const Wrapper = ({children}) => (
-    <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
-  )
-  return render(ui, {wrapper: Wrapper, ...options})
-}
-
 test('renders with the light styles for the light theme', () => {
-  renderWithProviders(<EasyButton>Easy</EasyButton>)
+  render(<EasyButton>Easy</EasyButton>, {theme: 'light'})
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: white;
@@ -24,9 +16,7 @@ test('renders with the light styles for the light theme', () => {
 })
 
 test('renders with the dark styles for the dark theme', () => {
-  renderWithProviders(<EasyButton>Easy</EasyButton>, {
-    theme: 'dark',
-  })
+  render(<EasyButton>Easy</EasyButton>, {theme: 'dark'})
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: black;
